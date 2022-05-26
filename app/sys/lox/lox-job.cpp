c0e26a6b8a8dfd919cdb6c7ac3717daad123511c
Tom.Leonard <Caireinc.com>
2022-05-26 13:55:09 -0400

    LOX code come in here

---
+++ app/sys/lox/lox-job.cpp
@@ -0,0 +1,17 @@
+extern "C"
+{
+    #include "sys.h"
+    #include "sys_wifi.h"
+}
+
+
+class LoxJobs
+{
+public : 
+    LoxJobs() {};
+    ~LoxJobs() {};
+    //IniitScales ();
+    //WeightScale();
+    // Do all LOX methods here....
+};
+
