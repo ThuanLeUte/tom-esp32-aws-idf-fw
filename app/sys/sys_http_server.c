9626e8517633dfa05ef1349ba82f34bd6cd4ae1d
Thuan Le <thuanle.ute@gmail.com>
2022-05-30 00:59:38 +0700

    merge: Webpage

--- app/sys/sys_http_server.c
+++ app/sys/sys_http_server.c
@@ -1,12 +1,16 @@
-/* Simple HTTP Server Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
+/**
+* @file       sys_http_server.c
+* @copyright  Copyright (C) 2021 Hydratech. All rights reserved.
+* @license    This project is released under the Hydratech License.
+* @version    01.00.00
+* @date       2021-08-13
+* @author     Thuan Le
+* @brief      System module to handle HTTP Server
+* @note       None
+* @example    None
 */
 
+/* Includes ----------------------------------------------------------------- */
 #include <esp_wifi.h>
 #include <esp_event.h>
 #include <esp_log.h>
@@ -22,45 +26,372 @@
 #include "sys_nvs.h"
 #include "sys_devcfg.h"
 
-/* A simple example that demonstrates how to create GET and POST
- * handlers for the web server.
- */
-httpd_handle_t server = NULL;
+/* Private enum/structs ----------------------------------------------------- */
+/* Private defines ---------------------------------------------------------- */
+static const char *TAG = "sys_http_server";
 
-static const char index_html_start[] asm("_binary_index_html_start");
-static const char index_html_end[] asm("_binary_index_html_end");
+/* Private Constants -------------------------------------------------------- */
+static const char file_login_html_start[]        asm("_binary_login_html_start");
+static const char file_login_html_end[]          asm("_binary_login_html_end");
+static const char file_main_page_html_start[]    asm("_binary_main_html_start");
+static const char file_main_page_html_end[]      asm("_binary_main_html_end");
 
-static esp_err_t hello_get_handler(httpd_req_t *req);
-static esp_err_t button_post_handler(httpd_req_t *req);
+static const char file_bootstrap_min_css_start[] asm("_binary_bootstrap_min_css_start");
+static const char file_bootstrap_min_css_end[]   asm("_binary_bootstrap_min_css_end");
+static const char file_pixie_main_css_start[]    asm("_binary_pixie_main_css_start");
+static const char file_pixie_main_css_end[]      asm("_binary_pixie_main_css_end");
 
-static const char *TAG = "example";
+static const char file_bootstrap_min_js_start[]  asm("_binary_bootstrap_min_js_start");
+static const char file_bootstrap_min_js_end[]    asm("_binary_bootstrap_min_js_end");
+static const char file_pixie_custom_js_start[]   asm("_binary_pixie_custom_js_start");
+static const char file_pixie_custom_js_end[]     asm("_binary_pixie_custom_js_end");
+static const char file_jquery_js_start[]         asm("_binary_jquery_min_js_start");
+static const char file_jquery_js_end[]           asm("_binary_jquery_min_js_end");
 
-static const httpd_uri_t hello = 
+
+static const char file_ap_img_start[]            asm("_binary_ap_png_start");
+static const char file_ap_img_end[]              asm("_binary_ap_png_end");
+static const char file_eye_close_img_start[]     asm("_binary_eye_close_png_start");
+static const char file_eye_close_img_end[]       asm("_binary_eye_close_png_end");
+static const char file_light_img_start[]         asm("_binary_light_png_start");
+static const char file_light_img_end[]           asm("_binary_light_png_end");
+static const char file_network_img_start[]       asm("_binary_network_png_start");
+static const char file_network_img_end[]         asm("_binary_network_png_end");
+static const char file_other_img_start[]         asm("_binary_other_png_start");
+static const char file_other_img_end[]           asm("_binary_other_png_end");
+static const char file_peripheral_img_start[]    asm("_binary_periperal_png_start");
+static const char file_peripheral_img_end[]      asm("_binary_periperal_png_end");
+static const char file_reboot_img_start[]        asm("_binary_reboot_png_start");
+static const char file_reboot_img_end[]          asm("_binary_reboot_png_end");
+static const char file_service_img_start[]       asm("_binary_service_png_start");
+static const char file_service_img_end[]         asm("_binary_service_png_end");
+static const char file_status_img_start[]        asm("_binary_status_png_start");
+static const char file_status_img_end[]          asm("_binary_status_png_end");
+static const char file_timezone_img_start[]      asm("_binary_timezone_png_start");
+static const char file_timezone_img_end[]        asm("_binary_timezone_png_end");
+static const char file_upgrade_img_start[]       asm("_binary_upgrade_png_start");
+static const char file_upgrade_img_end[]         asm("_binary_upgrade_png_end");
+
+/* Public variables --------------------------------------------------------- */
+/* Private function prototypes ---------------------------------------------- */
+static httpd_handle_t start_webserver(void);
+static esp_err_t web_login_html_handler(httpd_req_t *req);
+static esp_err_t web_main_page_html_handler(httpd_req_t *req);
+static esp_err_t web_button_config_handler(httpd_req_t *req);
+
+static esp_err_t web_bootstrap_min_css_handler(httpd_req_t *req);
+static esp_err_t web_pixie_main_css_handler(httpd_req_t *req);
+
+static esp_err_t web_bootstrap_min_js_handler(httpd_req_t *req);
+static esp_err_t web_pixie_custom_js_handler(httpd_req_t *req);
+static esp_err_t web_jquery_js_handler(httpd_req_t *req);
+
+static esp_err_t web_ap_img_handler(httpd_req_t *req);
+static esp_err_t web_eye_close_img_handler(httpd_req_t *req);
+static esp_err_t web_light_img_handler(httpd_req_t *req);
+static esp_err_t web_network_img_handler(httpd_req_t *req);
+static esp_err_t web_other_img_handler(httpd_req_t *req);
+static esp_err_t web_peripheral_img_handler(httpd_req_t *req);
+static esp_err_t web_reboot_img_handler(httpd_req_t *req);
+static esp_err_t web_service_img_handler(httpd_req_t *req);
+static esp_err_t web_status_img_handler(httpd_req_t *req);
+static esp_err_t web_timezone_img_handler(httpd_req_t *req);
+static esp_err_t web_upgrade_img_handler(httpd_req_t *req);
+
+/* Private variables -------------------------------------------------------- */
+static httpd_handle_t m_server = NULL;
+
+static const httpd_uri_t web_login = 
 {
-    .uri      = "/config",
+    .uri      = "/login",
     .method   = HTTP_GET,
-    .handler  = hello_get_handler,
+    .handler  = web_login_html_handler,
     .user_ctx = NULL
 };
 
-static const httpd_uri_t button = 
+static const httpd_uri_t web_main_page = 
 {
-    .uri      = "/setup_btn",
+    .uri      = "/main.html",
+    .method   = HTTP_GET,
+    .handler  = web_main_page_html_handler,
+    .user_ctx = NULL
+};
+
+static const httpd_uri_t web_button_config = 
+{
+    .uri      = "/button_config",
     .method   = HTTP_POST,
-    .handler  = button_post_handler,
+    .handler  = web_button_config_handler,
     .user_ctx = NULL
 };
 
-/* An HTTP GET handler */
-static esp_err_t hello_get_handler(httpd_req_t *req)
+static httpd_uri_t web_bootstrap_min_css = 
 {
-  httpd_resp_send(req, index_html_start, index_html_end - index_html_start);
+    .uri      = "/web/css/bootstrap.min.css",
+    .method   = HTTP_GET,
+    .handler  = web_bootstrap_min_css_handler,
+    .user_ctx = NULL
+};
 
+static httpd_uri_t web_pixie_main_css =
+{
+    .uri      = "/web/css/pixie-main.css",
+    .method   = HTTP_GET,
+    .handler  = web_pixie_main_css_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_bootstrap_min_js = 
+{
+    .uri      = "/web/js/bootstrap.min.js",
+    .method   = HTTP_GET,
+    .handler  = web_bootstrap_min_js_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_pixie_custom_js =
+{
+    .uri      = "/web/js/pixie-custom.js",
+    .method   = HTTP_GET,
+    .handler  = web_pixie_custom_js_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_jquery_js = 
+{
+    .uri      = "/web/js/jquery.min.js",
+    .method   = HTTP_GET,
+    .handler  = web_jquery_js_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_ap_img = 
+{
+    .uri      = "/web/images/ap.png",
+    .method   = HTTP_GET,
+    .handler  = web_ap_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_eye_close_img = 
+{
+    .uri      = "/web/images/eye-close.png",
+    .method   = HTTP_GET,
+    .handler  = web_eye_close_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_light_img = 
+{
+    .uri      = "/web/images/light.png",
+    .method   = HTTP_GET,
+    .handler  = web_light_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_network_img = 
+{
+    .uri      = "/web/images/network.png",
+    .method   = HTTP_GET,
+    .handler  = web_network_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_other_img = 
+{
+    .uri      = "/web/images/other.png",
+    .method   = HTTP_GET,
+    .handler  = web_other_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_peripheral_img = 
+{
+    .uri      = "/web/images/peripheral.png",
+    .method   = HTTP_GET,
+    .handler  = web_peripheral_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_reboot_img = 
+{
+    .uri      = "/web/images/reboot.png",
+    .method   = HTTP_GET,
+    .handler  = web_reboot_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_service_img = 
+{
+    .uri      = "/web/images/service.png",
+    .method   = HTTP_GET,
+    .handler  = web_service_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_status_img = 
+{
+    .uri      = "/web/images/status.png",
+    .method   = HTTP_GET,
+    .handler  = web_status_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_timezone_img = 
+{
+    .uri      = "/web/images/timezone.png",
+    .method   = HTTP_GET,
+    .handler  = web_timezone_img_handler,
+    .user_ctx = NULL
+};
+
+static httpd_uri_t web_upgrade_img = 
+{
+    .uri      = "/web/images/upgrade.png",
+    .method   = HTTP_GET,
+    .handler  = web_upgrade_img_handler,
+    .user_ctx = NULL
+};
+
+/* Function definitions ----------------------------------------------------- */
+void sys_http_server_stop(void)
+{
+  httpd_stop(m_server);
+}
+
+void sys_http_server_start(void)
+{
+  m_server = start_webserver();
+}
+
+/* Private function definitions --------------------------------------------- */
+static esp_err_t web_bootstrap_min_css_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/css");
+  httpd_resp_send(req, (const char *)file_bootstrap_min_css_start, file_bootstrap_min_css_end - file_bootstrap_min_css_start);
   return ESP_OK;
 }
 
-/* An HTTP POST handler */
-static esp_err_t button_post_handler(httpd_req_t *req)
+static esp_err_t web_pixie_main_css_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/css");
+  httpd_resp_send(req, (const char *)file_pixie_main_css_start, file_pixie_main_css_end - file_pixie_main_css_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_login_html_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/html");
+  httpd_resp_send(req, file_login_html_start, file_login_html_end - file_login_html_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_main_page_html_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/html");
+  httpd_resp_send(req, file_main_page_html_start, file_main_page_html_end - file_main_page_html_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_bootstrap_min_js_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/javascript");
+  httpd_resp_send(req, file_bootstrap_min_js_start, file_bootstrap_min_js_end - file_bootstrap_min_js_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_pixie_custom_js_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/javascript");
+  httpd_resp_send(req, file_pixie_custom_js_start, file_pixie_custom_js_end - file_pixie_custom_js_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_jquery_js_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "text/javascript");
+  httpd_resp_send(req, file_jquery_js_start, file_jquery_js_end - file_jquery_js_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_ap_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_ap_img_start, file_ap_img_end - file_ap_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_eye_close_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_eye_close_img_start, file_eye_close_img_end - file_eye_close_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_light_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_light_img_start, file_light_img_end - file_light_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_network_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_network_img_start, file_network_img_end - file_network_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_other_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_other_img_start, file_other_img_end - file_other_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_peripheral_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_peripheral_img_start, file_jquery_js_end - file_peripheral_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_reboot_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_jquery_js_start, file_peripheral_img_end - file_jquery_js_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_service_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_service_img_start, file_service_img_end - file_service_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_status_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_status_img_start, file_status_img_end - file_status_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_timezone_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_timezone_img_start, file_timezone_img_end - file_timezone_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_upgrade_img_handler(httpd_req_t *req)
+{
+  httpd_resp_set_type(req, "image/png");
+  httpd_resp_send(req, file_upgrade_img_start, file_upgrade_img_end - file_upgrade_img_start);
+  return ESP_OK;
+}
+
+static esp_err_t web_button_config_handler(httpd_req_t *req)
 {
   char buf[500];
   int ret = 0;
@@ -136,31 +467,44 @@ static esp_err_t button_post_handler(httpd_req_t *req)
 
 static httpd_handle_t start_webserver(void)
 {
-  httpd_handle_t server = NULL;
   httpd_config_t config = HTTPD_DEFAULT_CONFIG();
   config.lru_purge_enable = true;
+  config.max_uri_handlers = 20;
 
   // Start the httpd server
   ESP_LOGI(TAG, "Starting server on port: '%d'", config.server_port);
-  if (httpd_start(&server, &config) == ESP_OK)
+  if (httpd_start(&m_server, &config) == ESP_OK)
   {
     // Set URI handlers
     ESP_LOGI(TAG, "Registering URI handlers");
-    httpd_register_uri_handler(server, &hello);
-    httpd_register_uri_handler(server, &button);
-    return server;
+    httpd_register_uri_handler(m_server, &web_login);
+    httpd_register_uri_handler(m_server, &web_main_page);
+    httpd_register_uri_handler(m_server, &web_button_config);
+
+    httpd_register_uri_handler(m_server, &web_bootstrap_min_css);
+    httpd_register_uri_handler(m_server, &web_pixie_main_css);
+
+    httpd_register_uri_handler(m_server, &web_bootstrap_min_js);
+    httpd_register_uri_handler(m_server, &web_pixie_custom_js);
+    httpd_register_uri_handler(m_server, &web_jquery_js);
+
+    httpd_register_uri_handler(m_server, &web_ap_img);
+    httpd_register_uri_handler(m_server, &web_eye_close_img);
+    httpd_register_uri_handler(m_server, &web_light_img);
+    httpd_register_uri_handler(m_server, &web_network_img);
+    httpd_register_uri_handler(m_server, &web_other_img);
+    httpd_register_uri_handler(m_server, &web_peripheral_img);
+    httpd_register_uri_handler(m_server, &web_reboot_img);
+    httpd_register_uri_handler(m_server, &web_service_img);
+    httpd_register_uri_handler(m_server, &web_status_img);
+    httpd_register_uri_handler(m_server, &web_timezone_img);
+    httpd_register_uri_handler(m_server, &web_upgrade_img);
+
+    return m_server;
   }
 
   ESP_LOGI(TAG, "Error starting server!");
   return NULL;
 }
 
-void sys_http_server_stop(void)
-{
-  httpd_stop(server);
-}
-
-void sys_http_server_start(void)
-{
-  server = start_webserver();
-}
+/* End of file -------------------------------------------------------------- */
