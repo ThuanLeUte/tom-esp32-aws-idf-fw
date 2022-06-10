4deb0d1fd6f59c25274ae2e2fb283a5eb95f2efa
Tom.Leonard <Caireinc.com>
2022-06-10 12:58:07 -0400

    added product build variants

--- app/sys/sys_variant.h
+++ app/sys/sys_variant.h
@@ -10,6 +10,10 @@
 #define __SYS_VARIANT_H
 
 #define __CONFIG_SOFT_AP_MODE   (1)    //eaable soft AP mode for xx mins after reset, mostly for development
+#define __CONFIG_BLE_CONFIG_MODE   (1)    //eaable BLE config mode for xx mins after reset, mostly for myCaire phone app
+#define __LOX_BUILD__           (0)     //enable build for LOX product
+#define __TPOC_BUILD__           (0)     //enable build for TPOC Concentrator product
+#define __GSOC_BUILD__           (0)     //enable build for Global SOC product
 
 
 #endif /* __SYS_VARIANT_H */
