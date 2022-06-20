7c519e5c4274883635d00ba2b3d67e653f607fd7
Tom.Leonard <Caireinc.com>
2022-06-20 09:53:47 -0400

    variant defines and descriptions

--- app/sys/sys_variant.h
+++ app/sys/sys_variant.h
@@ -9,11 +9,12 @@
 #ifndef __SYS_VARIANT_H
 #define __SYS_VARIANT_H
 
-#define __CONFIG_SOFT_AP_MODE   (1)    //eaable soft AP mode for xx mins after reset, mostly for development
-#define __CONFIG_BLE_CONFIG_MODE   (1)    //eaable BLE config mode for xx mins after reset, mostly for myCaire phone app
-#define __LOX_BUILD__           (0)     //enable build for LOX product
-#define __TPOC_BUILD__           (0)     //enable build for TPOC Concentrator product
-#define __GSOC_BUILD__           (0)     //enable build for Global SOC product
+#define __CONFIG_SOFT_AP_MODE (1)    // eaable soft AP mode for xx mins after reset, mostly for development
+#define __CONFIG_BLE_CONFIG_MODE (1) // eaable BLE config mode for xx mins after reset, mostly for myCaire phone app
+// these next product defines are mutually exclusive , select only one type and make otheres '0'
+#define __LOX_BUILD__ (1)            // enable build for LOX product
+#define __TPOC_BUILD__ (0)           // enable build for TPOC Concentrator product
+#define __GSOC_BUILD__ (0)           // enable build for Global SOC product
 
 
 #endif /* __SYS_VARIANT_H */
